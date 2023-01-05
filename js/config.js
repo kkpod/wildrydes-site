5af214953c3b59aa46f92ca55bead836b1ef6aa1
kkpod <kranti.podupu@gmail.com>
2023-01-05 22:50:15 +0000

    Cog Config

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
-    cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
-    },
-    api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
+	    cognito: {
+		            userPoolId: 'us-east-1_SccQ2r6VC', // e.g. us-east-2_uXboG5pAb
+		            userPoolClientId: '17vfo2hev6727pde8a276terl4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+		            region: 'us-east-1' // e.g. us-east-2
+		        },
+	    api: {
+		            invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+		        }
 };
+
