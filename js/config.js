e2935bdf49c030d0fce696e1249f2c7bd3a7b84d
kkpod <kranti.podupu@gmail.com>
2023-01-07 21:51:36 +0000

    User Pool

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 window._config = {
-	    cognito: {
-		            userPoolId: 'us-east-1_SccQ2r6VC', // e.g. us-east-2_uXboG5pAb
-		            userPoolClientId: '17vfo2hev6727pde8a276terl4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-		            region: 'us-east-1' // e.g. us-east-2
-		        },
-	    api: {
-		    invokeUrl: 'https://0c7eeeqf35.execute-api.us-east-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-		        }
+    cognito: {
+ 		userPoolId: 'us-east-1_ASa7q7y5v', // e.g. us-east-2_uXboG5pAb
+		userPoolClientId: '4cnmvl594t8i492b7kt7p5d7r1', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+		region: 'us-east-1' // e.g. us-east-2
+	},
+    api: {
+	    invokeUrl: ''
+	    // invokeUrl: 'https://0c7eeeqf35.execute-api.us-east-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+	}
 };
 
